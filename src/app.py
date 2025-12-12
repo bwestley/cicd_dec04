0bbf69151725048050a91f86259ef6f17f6e26b6
Brendan Westley <brendan@westley.us>
2025-12-11 23:16:42 -0600

    Add features and tests

--- src/app.py
+++ src/app.py
@@ -1,5 +1,33 @@
-def add (a, b):
-    return a+b
+import math
 
-def sub (a, b):
-    return a-b
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+def log(n):
+    return math.log(n)
+
+
+def cos(n):
+    return math.cos(n)
+
+
+def sqrt(n):
+    return math.sqrt(n)
+
+
+def percentage(a, b):
+    return a / b * 100
